b4e0ce9cc07114d86afbec3932eae3d3541e48c0
faisal-dev <faisal.jeaumar@gmail.com>
2015-10-23 05:35:18 +0700

    Setting Preloader
    ADD | Preloader page & JS script
    ADD | Animated css dependencies
    ADD | variables, mixins, type, page loader CSS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+(function($) {
+  'use strict';
+
+  // Preloading
+  $(window).load(function() {
+    // Wait 5s to fade out page loader
+    setTimeout(function() {
+      $('#loader-name').addClass('animated fadeOutLeft')
+      $('#loader-job').addClass('animated fadeOutRight')
+      $('#page-loader')
+        .addClass('animated fadeOut')
+        .fadeOut(400, function() {
+          return;
+        });
+    }, 5000);
+  });
+
+})(jQuery);
